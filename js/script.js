f566213de98924fdc1c536b5d4a710a5215be806
Sriharsha Guduguntla <sguduguntla11@gmail.com>
2017-07-17 14:03:25 -0700

    Redesigned entire skills page and added hobbies section

--- js/script.js
+++ js/script.js
@@ -1,33 +1,38 @@
 $(document).ready(function() {
 
-  //Mobile "Add to Homescreen" alert activated
-  addToHomescreen({
-    skipFirstVisit: true,
-    maxDisplayCount: 1
-  });
+    //Mobile "Add to Homescreen" alert activated
+    addToHomescreen({
+        skipFirstVisit: true,
+        maxDisplayCount: 1
+    });
 
-  /*************************/
+    /*************************/
 
-  //Tooltips are activated
-  $('.tooltipped').tooltip({
-    delay: 50
-  });
+    //Tooltips are activated
+    $('.tooltipped').tooltip({
+        delay: 50
+    });
 
-  /*************************/
+    /*************************/
 
-  //Side-nav comes out from the right
-  $(".button-collapse").sideNav({
-    edge: 'right'
-  });
+    //Side-nav comes out from the right
+    $(".button-collapse").sideNav({
+        edge: 'right'
+    });
 
-  var win = $(window);
+    $('.collapsible').collapsible();
 
-  $(".welcomeTyper").typed({
-    strings: ["Hi. I'm Sriharsha.", "I believe that...", "<span style='font-size: 0.7em;'>\"In order to succeed, we must first believe we can.\"</span>", "I am a....",
-      "High Schooler", "Web Developer", "Music Lover", "Java Developer", "Singer", "Designer", "Chess Player", "Tech Fanatic", "Cello Player", "And last, but not least....", "an Award-Winning Hacker"
-    ],
-    typeSpeed: 0,
-    loop: true
-  });
+
+    var win = $(window);
+
+    $(".welcomeTyper").typed({
+        strings: ["Hi. I'm Sriharsha.", "I believe that...", "<span style='font-size: 0.7em;'>\"In order to succeed</span>", "<span style='font-size: 0.7em;'>we must first believe we can.\"</span>", "I am a....",
+            "High Schooler", "Web Developer", "Music Lover", "Java Developer", "Singer", "Designer", "Chess Player", "Tech Fanatic", "Cello Player", "And last, but not least....", "<span style='font-size: 0.7em;'>an Award-Winning Hacker</span>"
+        ],
+        typeSpeed: 0,
+        loop: true
+    });
+
+
 
 });
