f6291681769459bd997c7cac1cfe999433bdf1ec
Sriharsha Guduguntla <sguduguntla11@gmail.com>
2021-09-17 15:25:30 -0700

    Push restaurant for Data-X

---
+++ restaurant/js/index.js
@@ -0,0 +1,131 @@
+(function () {
+  // imgDiv = document.getElementById("images");
+  // images = [
+  //   {
+  //     name: "Cheese Pizza",
+  //     lowestScale: 0.0625,
+  //     rOff: "(6, 1)",
+  //     bOff: "(3, -2)",
+  //   },
+  //   {
+  //     name: "Pepperoni Pizza",
+  //     lowestScale: 0.0625,
+  //     rOff: "(7, 1)",
+  //     bOff: "(-5, -2)",
+  //   },
+  //   {
+  //     name: "tobolsk.jpg",
+  //     lowestScale: 0.0625,
+  //     rOff: "(4, 1)",
+  //     bOff: "(-3, -3)",
+  //   },
+  //   {
+  //     name: "lady.jpg",
+  //     lowestScale: 0.25,
+  //     rOff: "(75, 2)",
+  //     bOff: "(-61, -7)",
+  //   },
+  //   {
+  //     name: "emir.jpg",
+  //     lowestScale: 0.25,
+  //     rOff: "(69, 17)",
+  //     bOff: "(-57, -25)",
+  //   },
+  //   {
+  //     name: "church.jpg",
+  //     lowestScale: 0.25,
+  //     rOff: "(35, -8)",
+  //     bOff: "(-25, -4)",
+  //   },
+  //   {
+  //     name: "icon.jpg",
+  //     lowestScale: 0.25,
+  //     rOff: "(57, 5)",
+  //     bOff: "(-45, -17)",
+  //   },
+  //   {
+  //     name: "melons.jpg",
+  //     lowestScale: 0.25,
+  //     rOff: "(105, 4)",
+  //     bOff: "(-97, -10)",
+  //   },
+  //   {
+  //     name: "onion_church.jpg",
+  //     lowestScale: 0.25,
+  //     rOff: "(71, 9)",
+  //     bOff: "(-63, -26)",
+  //   },
+  //   {
+  //     name: "self_portrait.jpg",
+  //     lowestScale: 0.25,
+  //     rOff: "(-93, -30)",
+  //     bOff: "(105, 8)",
+  //   },
+  //   {
+  //     name: "three_generations.jpg",
+  //     lowestScale: 0.25,
+  //     rOff: "(-65, -14)",
+  //     bOff: "(73, 0)",
+  //   },
+  //   {
+  //     name: "train.jpg",
+  //     lowestScale: 0.25,
+  //     rOff: "(51, 25)",
+  //     bOff: "(-49, -6)",
+  //   },
+  //   {
+  //     name: "workshop.jpg",
+  //     lowestScale: 0.25,
+  //     rOff: "(63, -10)",
+  //     bOff: "(-63, -1)",
+  //   },
+  //   {
+  //     name: "siren.jpg",
+  //     lowestScale: 0.25,
+  //     rOff: "(55, -18)",
+  //     bOff: "(-57, 5)",
+  //   },
+  //   {
+  //     name: "malorossii.jpg",
+  //     lowestScale: 0.25,
+  //     rOff: "(-12, 2)",
+  //     bOff: "(22, -8)",
+  //   },
+  //   {
+  //     name: "seeds.jpg",
+  //     lowestScale: 0.25,
+  //     rOff: "(83, -49)",
+  //     bOff: "(-45, 43)",
+  //   },
+  // ];
+  // images.map((img, i) => {
+  //   imgDiv.innerHTML += `<div class="col">
+  //                           <div class="card shadow-sm">
+  //                               <img src="img/orig_${img.name}" class="card-img-top" alt="...">
+  //                               <div class="card-body">
+  //                                   <h3 class="card-text lead fw-bold"><mark>${img.name}</mark>, BEFORE</h3>
+  //                               </div>
+  //                           </div>
+  //                       </div>
+  //                       <div class="col">
+  //                           <div class="card shadow-sm">
+  //                               <img src="img/${img.name}" class="card-img-top" alt="...">
+  //                               <div class="card-body">
+  //                                   <h3 class="card-text lead fw-bold"><mark>${img.name}</mark>, AFTER,
+  //                                       <code>PyramidSearch(lowest_scale=${img.lowestScale})</code>
+  //                                   </h3>
+  //                                   <hr />
+  //                                   <p class="card-text lead fw-bold"><span class="text-danger">Red (R)
+  //                                           Offsets:</span> <code>${img.rOff}</code>
+  //                                   </p>
+  //                                   <p class="card-text lead fw-bold"><span class="text-primary">Blue (B)
+  //                                           Offsets:</span>
+  //                                       <code>${img.bOff}</code> </p>
+  //                               </div>
+  //                           </div>
+  //                       </div>`;
+  //   if (i === 12) {
+  //     imgDiv.innerHTML += `<div class="col text-center"><h1 class="display-5 fw-bold">Extra Examples</h1></div><div class="col"><br/></div></div>`;
+  //   }
+  // });
+})();
